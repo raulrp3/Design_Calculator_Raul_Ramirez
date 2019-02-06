b7d61010d6cedfda10f198921058a300f0637ec0
Raul Ramirez <raul.ramirez@escuelaestech.es>
2019-02-06 10:50:48 +0100

    start main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,74 @@
+var divPn = "",firstNumber = 0,secondNumber = 0,operation = "",isDecimal = false;;
 window.onload = function(){
-		
+	var screen = document.getElementById("screen");
+	var divAdvanced = document.getElementById("pnAdvanced");
+	var divConversion = document.getElementById("pnConversion");
+	var normal = document.getElementById("normal");
+	var advanced = document.getElementById("advanced");
+	var conversion = document.getElementById("conversion");
+	var numbers = document.getElementById("buttons").getElementsByTagName("button");
+	var operations = document.getElementById("operations").getElementsByTagName("button");
+	var same = document.getElementById("same");
+	divAdvanced.style.display = "none";	
+	divConversion.style.display = "none";
+	normal.style.visibility = "hidden";
+	normal.addEventListener("click",function(){showNormal()});
+	advanced.addEventListener("click",function(){showAdvanced(divAdvanced,normal)});
+	conversion.addEventListener("click",function(){showConversion(divConversion,normal)});
+	for (var i = 0;i < numbers.length;i++){
+		numbers[i].addEventListener("click",function(){showNumber(screen)});
+	}
+	for (var i = 0;i < operations.length;i++){
+		operations[i].addEventListener("click",function(){showOperation(screen)});
+	}
+	same.addEventListener("click",function(){calculate(screen)});
+}
+function showNormal(){
+	document.getElementById(divPn).style.display = "none";
+	document.getElementById(event.target.id).style.visibility = "hidden";
+}
+function showAdvanced(divAdvanced,normal){
+	if (divAdvanced.style.display == "none"){
+		divAdvanced.style.display = "block";
+		normal.style.visibility = "visible";
+		divPn = divAdvanced.id;
+	}
+}
+function showConversion(divConversion,normal){
+	if (divConversion.style.display == "none"){
+		divConversion.style.display = "block";
+		normal.style.visibility = "visible";
+		divPn = divConversion.id;
+	}
+}
+function showNumber(screen){
+	screen.value += event.target.name;
+}
+function showOperation(screen){
+	switch (event.target.name){
+		case "AC":{
+			firstNumber = 0;
+			secondNumber = 0;
+			operation = "";
+		}
+		break;
+		case ".":{
+			screen.value += event.target.name;
+			isDecimal = true;
+		}
+		break;
+		default:{
+			firstNumber = screen.value;
+			operation = event.target.name;
+			screen.value = "";
+		}
+		break;
+	}
+}
+function calculate(screen){
+	secondNumber = screen.value;
+	if (isDecimal){
+		firstNumber = parseFloat(firstNumber);
+		secondNumber = parseFloat(secondNumber);
+	}
 }
